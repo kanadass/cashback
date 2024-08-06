35809debf93b27322261eb3bcf9307b175685bcc
Vitalii Mikhaylov <kanada109@gmail.com>
2024-08-06 14:11:28 +0300

    added css

--- cashback/cashback_site/urls.py
+++ cashback/cashback_site/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,6 +24,6 @@ from cashback.views import page_not_found
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cashback.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = page_not_found
